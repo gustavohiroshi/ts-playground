8598071841b61afc00e1db3e335e17751f188050
gustavohiroshi <72774786+gustavohiroshi@users.noreply.github.com>
2022-10-02 17:45:29 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('teste');
+const Guard = {
+  isNullable: (value: unknown) => {
+    if (value == null || value == undefined) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+};
+
+console.log(Guard.isNullable(null));
